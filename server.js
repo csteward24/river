f677fda5b8aece4759ec38c0c979a350e5864fb8
cadence <csteward24@gmail.com>
2020-04-27 16:43:45 -0400

    clean up add logging

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.set('view engine', 'pug');
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-//open_db_connection(db_con);
-//render_page(db_con);
+update_USGS();
 update_OWM();
 //routes
 app.get('/', function (req, res) {
@@ -27,30 +26,26 @@ app.get('/', function (req, res) {
     let location_data_promise = db_con.then(db => db.query(`SELECT DISTINCT location FROM temp ORDER BY time DESC`));
     Promise.all([water_data_promise,air_data_promise,location_data_promise]).then(data => {
         data = [data[0][0],data[1][0],data[2]];
-        console.log(data[2]);
         res.render('river', { title: 'River', source: data[0].location,
-            water_temp: data[0].value, air_temp: data[1].temp, weather: data[1].weather, Locations:data[2]});},
+            water_temp: data[0].value, air_temp: data[1].temp, weather: data[1].weather, Locations:data[2]});
+        console.log("page rendered")},
         error => {console.log("error: ", error)})
 
 });
 
-function update_USGS(connection){
-    const url = "https://waterservices.usgs.gov/nwis/iv/?format=json&countyCd=42101&parameterCd=00010&siteType=ST&siteStatus=all"
-    fetch(url).then(x => {return x.json()}).then(
-        data => {
-            var vals = [];
-            data.value.timeSeries.forEach(x => {vals.push([x.sourceInfo.siteName,x.values[0].value[0].value,x.values[0].value[0].dateTime])});
-            const sql = "INSERT INTO temp(location,value,time) VALUES ?";
-            connection.query(sql, [vals],function (err, result) {
-                if (err) throw err;
-                console.log("values inserted");
-            });
-
-        });
+function update_USGS(){
+    console.log("USGS data updated");
+    const url = "https://waterservices.usgs.gov/nwis/iv/?format=json&countyCd=42101&parameterCd=00010&siteType=ST&siteStatus=all";
+    let json_promise = fetch(url).then(x => {return x.json()});
+    Promise.all([json_promise,db_con]).then(data => {
+        var vals = [];
+        data[0].value.timeSeries.forEach(x => {vals.push([x.sourceInfo.siteName,x.values[0].value[0].value,x.values[0].value[0].dateTime])});
+        data[1].query("INSERT INTO temp(location,value,time) VALUES ?",[vals]);
+    })
 }
-
 function update_OWM(){
-    const url = 'http://api.openweathermap.org/data/2.5/group?id=5205788&units=imperial&APPID=ffd8afa9a03c59a8124a9e28b21048c9'
+    console.log("OWM data updated");
+    const url = 'http://api.openweathermap.org/data/2.5/group?id=5205788&units=imperial&APPID=ffd8afa9a03c59a8124a9e28b21048c9';
     let json_promise = fetch(url).then(x => {return x.json()});
     Promise.all([json_promise,db_con]).then(data => {
         var vals = [];
